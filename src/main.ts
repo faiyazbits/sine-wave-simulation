43f758906a62fb5489f6d19e4ef8f366c151a8eb
faiyazbits <m.faiyaz123@gmail.com>
2022-04-01 17:20:05 +0530

    added pencil

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,11 @@
 import './style.css'
+import { Scene, Circle } from "pencil.js";
 
+const scene = new Scene();
+const radius = 30;
+const circle = new Circle(scene.center, radius )
+
+scene.add(circle);
+
+circle.draggable();
+scene.startLoop();
